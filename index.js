c823d21a64ebcacdcaaefa113352cefaadc4a4c1
Varvara Babkova <varvara.munday@gmail.com>
2019-08-06 09:29:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 /* Given Code, don't edit */
 
-function handleClick(e) {
+function handleClick() {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
 }
 
-/* End Given Code, don't edit above here...*/
-
 /* Write your implementation of greet() */
+function greet(time){
+	// hour = 0;
+	const hour = parseInt(time.split(":")[0]);
+	// debugger;
+	if (hour < 12)
+		return "Good Morning";
+	if (hour >= 17)
+		return "Good Evening";
+	if (hour >= 12)
+		return "Good Afternoon";
+	// return "Good Morning";
+}
 /* Write your implementation of displayMessage() */
+function displayMessage( str) {
+	let gr = document.getElementById("greeting");
+	gr.innerText = str;
+}
